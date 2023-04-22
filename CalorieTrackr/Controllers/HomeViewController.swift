885d831302bd5524beccdeddef2325685f3b003a
Andrei Baluta <andrei.baluta@yahoo.com>
2023-04-22 19:03:11 +0300

    Fixed stuff, working version before adding auth functionality

---
+++ CalorieTrackr/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  CalorieTrackr
+//
+//  Created by Andrei Baluta on 22.04.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
