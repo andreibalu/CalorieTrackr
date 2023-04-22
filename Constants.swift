77bb72477817498fc803a80d22be25f57d99ae94
Andrei Baluta <andrei.baluta@yahoo.com>
2023-04-22 15:58:39 +0300

    Added constants file

---
+++ Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  CalorieTrackr
+//
+//  Created by Andrei Baluta on 22.04.2023.
+//
+
+struct K {
+    struct AppColors {
+        static let purple = "ColorPurple"
+        static let pink = "ColorPink"
+        static let navy = "ColorNavy"
+        static let crem = "ColorCrem"
+    }
+}
