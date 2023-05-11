06ec2345493f27274631c19386a972ad84f51765
Andrei Baluta <andrei.baluta@yahoo.com>
2023-05-12 02:05:52 +0300

    Added healthkit and asking for permissions for reading and writing calories

--- CalorieTrackr/Controllers/HomeViewController.swift
+++ CalorieTrackr/Controllers/HomeViewController.swift
@@ -4,12 +4,10 @@
 //
 //  Created by Andrei Baluta on 22.04.2023.
 //
-
 import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
-
 //    @IBAction func logoutPressed(_ sender: UIButton) {
 //        do {
 //            try Auth.auth().signOut()
@@ -20,17 +18,34 @@ import FirebaseAuth
 //    }
 //    must be moved to settings screen
 
+import HealthKit
+
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if HKHealthStore.isHealthDataAvailable() {
+            print("yes")
+            let healthStore = HKHealthStore()
+            let allTypes = Set([HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!])
+
+            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+                if !success {
+                    let alert = UIAlertController(title: "HealthKit error", message: error?.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+        
         
         // Disabling nav controller
         guard let navigationController = navigationController else { return }
         let viewControllers = navigationController.viewControllers.filter { $0 != self }
         navigationController.setViewControllers(viewControllers, animated: false)
     }
+    
 }
 
 
