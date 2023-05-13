d9aa7aa90d930429fef60c8689dcdbc9d8b0f665
Andrei Baluta <andrei.baluta@yahoo.com>
2023-05-13 20:43:52 +0300

    Fixed logout functionality and storyboard structure for the home storyboard. We are going to use a tab bar controller linked to a navigation controller for each of its tabs

--- CalorieTrackr/Controllers/SettingsViewController.swift
+++ CalorieTrackr/Controllers/SettingsViewController.swift
@@ -10,10 +10,15 @@ import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
+protocol TabBarDelegate: AnyObject {
+    func logoutAndNavigateToWelcome()
+}
+
 class SettingsViewController: UIViewController {
     
     let db = Firestore.firestore()
     @IBOutlet weak var titleLabel: UILabel!
+    weak var tabBarDelegate: TabBarDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +28,7 @@ class SettingsViewController: UIViewController {
         print("apasat")
         do {
             try Auth.auth().signOut()
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            if let welcomeViewController = storyboard.instantiateViewController(withIdentifier: K.logout) as? WelcomeViewController {
-                // Set up the navigation controller with the login view controller
-                let navController = UINavigationController(rootViewController: welcomeViewController)
-                navController.modalPresentationStyle = .fullScreen
-                
-                // Replace the current view controllers with the login view controller
-                self.navigationController?.setViewControllers([welcomeViewController], animated: true)
-            }
+            tabBarDelegate?.logoutAndNavigateToWelcome()
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
--- CalorieTrackr/Controllers/TabBarHomeController.swift
+++ CalorieTrackr/Controllers/TabBarHomeController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class TabBarHomeController: UITabBarController{
+class TabBarHomeController: UITabBarController, TabBarDelegate{
+    func logoutAndNavigateToWelcome() {
+        self.tabBar.isUserInteractionEnabled = false
+        //Go to WelcomeVC
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let welcomeViewController = storyboard.instantiateViewController(withIdentifier: K.logout) as? WelcomeViewController {
+            let navController = UINavigationController(rootViewController: welcomeViewController)
+            navController.modalPresentationStyle = .fullScreen
+
+            // Replace the current view controllers with the welcome view controller
+            self.navigationController?.setViewControllers([welcomeViewController], animated: true)
+        }
+    }
+    
     @IBInspectable var initialIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedIndex = initialIndex
         navigationItem.hidesBackButton = true
+        
+        // Set self as the delegate for the SettingsVC after finding it
+        guard let viewControllers = viewControllers else { return }
+        for viewController in viewControllers {
+            if let navController = viewController as? UINavigationController {
+                for childViewController in navController.viewControllers {
+                    if let SettingsViewController = childViewController as? SettingsViewController {
+                        SettingsViewController.tabBarDelegate = self
+                    }
+                }
+            }
+        }
     }
 }
