50bb05aedaf6edebe43f37d9adcbafb78ac79df3
Tatucu <andreid2312@gmail.com>
2023-05-20 15:47:10 +0300

    Apple Watch Intergration

---
+++ CalorieTrackrWatch/CalorieTrackrWatch.swift
@@ -0,0 +1,16 @@
+//
+//  CalorieTrackrWatch.swift
+//  CalorieTrackrWatch
+//
+//  Created by Andrei Tatucu on 19.05.2023.
+//
+
+import AppIntents
+
+struct CalorieTrackrWatch: AppIntent {
+    static var title: LocalizedStringResource = "CalorieTrackrWatch"
+    
+    func perform() async throws -> some IntentResult {
+        return .result()
+    }
+}
---
+++ CalorieTrackrWatch/CalorieTrackrWatchExtension.swift
@@ -0,0 +1,12 @@
+//
+//  CalorieTrackrWatchExtension.swift
+//  CalorieTrackrWatch
+//
+//  Created by Andrei Tatucu on 19.05.2023.
+//
+
+import AppIntents
+
+@main
+struct CalorieTrackrWatchExtension: AppIntentsExtension {
+}
