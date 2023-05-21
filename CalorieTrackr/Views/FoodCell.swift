5ad86dd47e71ee0116144ab05d41dc42ff47f479
Andrei Baluta <andrei.baluta@yahoo.com>
2023-05-21 22:45:34 +0300

    Created message cell class

---
+++ CalorieTrackr/Views/FoodCell.swift
@@ -0,0 +1,23 @@
+//
+//  FoodCell.swift
+//  CalorieTrackr
+//
+//  Created by Andrei Baluta on 21.05.2023.
+//
+
+import UIKit
+
+class FoodCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
